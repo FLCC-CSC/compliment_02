a581e9e6c073ddac59fbc32aff32e339df43c19f
daveflcc <143441096+daveflcc@users.noreply.github.com>
2025-02-26 06:51:01 -0500

    Create compliment_02.py

---
+++ compliment_02.py
@@ -0,0 +1,81 @@
+# FILE NAME - compliment_0
+2.py
+
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+Would you like a compliment? yes
+You have wonderful eyes.
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? Yes
+No compliment for you!
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? y
+No compliment for you!
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? no
+No compliment for you!
+Thank you for playing.
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. Did you struggle with this lab (YES/NO)?
+
+
+
+
+
+
+
+'''
